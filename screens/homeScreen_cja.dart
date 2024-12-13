e25058c64dbaa190dd1078d8e06548d5d78400f2
AngelCJ <acontreras185@ieszaidinvergeles.org>
2024-12-13 09:01:51 +0100

    created homescreen

---
+++ screens/homeScreen_cja.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class homeScreen_cja extends StatelessWidget {
+  const homeScreen_cja({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
